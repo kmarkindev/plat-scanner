ea3152539a90832bf56b30259b7cc533c970bd63
Kirill Markin <kmarkin@mbsicorp.com>
2022-03-13 18:32:18 +0300

    Implement RelicDatabaseCreator

--- Sources/Services/RelicDatabase/RelicDatabaseCreator.cpp
+++ Sources/Services/RelicDatabase/RelicDatabaseCreator.cpp
@@ -1,1 +1,15 @@
 #include "RelicDatabaseCreator.h"
+
+ps::RelicDatabaseCreator::RelicDatabaseCreator(const std::string& lang, const std::string& platform)
+    : _api(lang, platform)
+{
+
+}
+
+ps::RelicItemsDatabase ps::RelicDatabaseCreator::CreateItemsDatabaseUsingWfApi()
+{
+    std::string apiResponse = _api.FetchAllTradeableItems();
+    auto db = _parser.ParseFromString(apiResponse);
+    _filter.FilterDatabase(db);
+    return db;
+}
--- Sources/Services/RelicDatabase/RelicDatabaseCreator.h
+++ Sources/Services/RelicDatabase/RelicDatabaseCreator.h
@@ -1,8 +1,23 @@
 #pragma once
 
-class RelicDatabaseCreator
-{
+#include <string>
+#include <Dto/RelicItemsDatabase.h>
+#include "RelicDatabaseJsonParser.h"
+#include <Services/WarframeMarketApi.h>
+#include "RelicDatabasePrimeItemsFilter.h"
 
-};
+namespace ps
+{
+    class RelicDatabaseCreator
+    {
+    public:
+        RelicDatabaseCreator(const std::string& lang, const std::string& platform);
+        RelicItemsDatabase CreateItemsDatabaseUsingWfApi();
+    private:
+        WarframeMarketApi _api;
+        RelicDatabaseJsonParser _parser;
+        RelicDatabasePrimeItemsFilter _filter;
+    };
+}
 
 
