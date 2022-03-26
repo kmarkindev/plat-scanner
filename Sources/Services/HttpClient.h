e12b2a1c1b6d909e20d479b913be1ae765456da6
Kirill Markin <kmarkindev@gmail.com>
2022-03-26 21:24:47 +0300

    Implement HttpClient

--- Sources/Services/HttpClient.h
+++ Sources/Services/HttpClient.h
@@ -1,20 +1,11 @@
 #pragma once
 
-#include <wx/wx.h>
-#include <wx/protocol/http.h>
-#include <wx/sstream.h>
+#include <httplib/httplib.h>
 #include <Models/HttpResponse.h>
 #include <Models/HttpRequest.h>
 
 class HttpClient
 {
 public:
-
-    void SetTimeout(int timeout);
-    [[nodiscard]] int GetTimeout() const;
-
     HttpResponse Send(const HttpRequest& request);
-
-private:
-    int _timeout;
 };
